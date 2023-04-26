adccaec89844a0c2eec59f8a445d10bd69adb806
MirelaScalabrini <90942816+MirelaScalabrini@users.noreply.github.com>
2023-04-26 13:53:04 +0000

    (26/04) Exercícios 03 e 04

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css'
+import './style.css'
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Exercicio01 from './exercicios/01'
 import Exercicio02 from './exercicios/02'
 import Home from './pages/Home'
+import Exercicio03 from './exercicios/03'
+import Exercicio04 from './exercicios/04'
 
 function App() {
 
@@ -23,8 +26,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/01" element={<Exercicio01 />}/>
           <Route path="/02" element={<Exercicio02 />} />
-          <Route path="/03" />
-          <Route path="/04" />
+          <Route path="/03" element={<Exercicio03 />} />
+          <Route path="/04" element={<Exercicio04 />}/>
           <Route path="/05" />
           <Route path="/06" />
         </Routes>
--- react-hooks/src/exercicios/04.jsx
+++ react-hooks/src/exercicios/04.jsx
@@ -2,14 +2,17 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares é o estado para este componente. Adicione useState para squares
-  const squares = Array(9).fill(null)
-
+  //const squares = Array(9).fill(null)
+  const [squares, setSquares] = React.useState(Array(9).fill(null))
   // 🐨 Precisaremos dos seguintes itens de estados derivados:
   // - nextValue ('X' ou 'O')
   // - winner ('X', 'O', ou null)
   // - status (`Vencedor: ${winner}`, `Deu velha!`, or `Próximo jogador: ${nextValue}`)
   // 💰 Os respectivos cálculos já estão prontos. Basta usar os utilitários 
   // mais abaixo no código para criar essas variáveis
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
 
   // Esta é a função que o manipulador de clique no quadrado irá chamar. `square`
   // deve ser um índice. Portanto, se você clicar sobre o quadrado central, o
@@ -27,16 +30,20 @@ function Board() {
     //
     // 🐨 faça uma cópia da matriz dos quadrados
     // 💰 `[...squares]` é do que você precisa!)
+    const squaresCopy = [...squares]
     
     // 🐨 ajuste o valor do quadrado que foi selecionado
     // 💰 `squaresCopy[square] = nextValue`
+    squaresCopy[square] = nextValue
     
     // 🐨 atribua a cópia à matriz dos quadrados
+    setSquares(squaresCopy)
   }
 
   function restart() {
     // 🐨 volte os quadrados ao estado inicial
     // 💰 `Array(9).fill(null)` é do que você precisa!
+    setSquares(Array(9).fill(null))
   }
 
   function renderSquare(i) {
@@ -50,7 +57,7 @@ function Board() {
   return (
     <div>
       {/* 🐨 coloque o status na div abaixo */}
-      <div className="status"></div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -70,6 +77,7 @@ function Board() {
         restart
       </button>
       <hr />
+      <div>{JSON.stringify(squares) }</div>
     </div>
   )
 }
@@ -85,11 +93,14 @@ function Game() {
 }
 
 function calculateStatus(winner, squares, nextValue) {
-  return winner
-    ? `Vencedor: ${winner}`
-    : squares.every(Boolean)
-    ? `Deu velha!`
-    : `Próximo jogador: ${nextValue}`
+  // return winner
+  //   ? `Vencedor: ${winner}`
+  //   : squares.every(Boolean)
+  //   ? `Deu velha!`
+  //   : `Próximo jogador: ${nextValue}`
+  if(winner) return `Vencedor: ${winner}`
+  else if(squares.every(Boolean)) return `Deu velha!`
+  else return `Próximo jogador: ${nextValue}`
 }
 
 function calculateNextValue(squares) {
